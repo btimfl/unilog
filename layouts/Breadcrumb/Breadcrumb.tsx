48f86cee52bde16f743beafca72e2897d4f00816
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-03-28 12:44:23 +0530

    rearrange code to separate view from business logic

--- layouts/Breadcrumb/Breadcrumb.tsx
+++ layouts/Breadcrumb/Breadcrumb.tsx
@@ -4,28 +4,11 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-import { formatText } from './text-format'
+import { useBreadcrumbs } from './hooks'
 
 export default function BreadcrumbComp() {
     const router = useRouter()
-
-    const generateBreadcrumbs = () => {
-        const asPathWithoutQuery = router.asPath.split('?')[0]
-        const asPathNestedRoutes = asPathWithoutQuery.split('/').filter((v) => v.length > 0)
-        const crumblist = asPathNestedRoutes.map((subpath, idx) => {
-            const href = '/' + asPathNestedRoutes.slice(0, idx + 1).join('/')
-
-            // 'AbcDef' to 'Abc Def' to ['Abc', 'Def']
-            const crumbs = (subpath.charAt(0) + subpath.slice(1).replace(/([A-Z])/g, ' $1')).split(' ')
-            const title = crumbs.map(formatText).join(' ')
-
-            return { href, title }
-        })
-
-        return [...crumblist]
-    }
-
-    const breadcrumbs = generateBreadcrumbs()
+    const breadcrumbs = useBreadcrumbs(router)
 
     return (
         <Breadcrumb spacing="8px" separator={<ChevronRightIcon color="gray.500" />}>
