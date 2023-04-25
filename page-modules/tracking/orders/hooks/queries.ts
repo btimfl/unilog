4f7b11133dfe2a43aa0f7e985cf5d8564aaf057e
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-04-25 13:28:24 +0530

    Provide from and to for all timelines

--- page-modules/tracking/orders/hooks/queries.ts
+++ page-modules/tracking/orders/hooks/queries.ts
@@ -1,13 +1,36 @@
 import { useQuery } from '@tanstack/react-query'
 import { fetchExtendedMetadata, fetchMetadata, fetchShipmentDetails } from 'apis/get'
 import { fetchShipments } from 'apis/post'
+import sub from 'date-fns/sub'
 
-import { Filters } from '../types/filters'
+import { Filters, TimelineParams } from '../types/filters'
+
+function getTimeline(timeline: TimelineParams, from: string, to: string): { from: string; to: string } {
+    switch (timeline) {
+        case 'custom':
+            return { from, to }
+        case 'last_7_days':
+            return {
+                from: sub(new Date(), { days: 7 }).toISOString().split('T')[0],
+                to: new Date().toISOString().split('T')[0],
+            }
+        case 'last_month':
+            return {
+                from: sub(new Date(), { days: 30 }).toISOString().split('T')[0],
+                to: new Date().toISOString().split('T')[0],
+            }
+        case 'last_90_days':
+            return {
+                from: sub(new Date(), { days: 90 }).toISOString().split('T')[0],
+                to: new Date().toISOString().split('T')[0],
+            }
+    }
+}
 
 export function useShipments(filters: Filters) {
     return useQuery({
         queryKey: ['shipments', filters],
-        queryFn: () => fetchShipments(filters),
+        queryFn: () => fetchShipments({ ...filters, ...getTimeline(filters.timeline, filters.from, filters.to) }),
         refetchOnWindowFocus: false,
         refetchInterval: (data, query) =>
             query.state.dataUpdateCount === 1 && data?.result?.refresh_required ? 100 : false,
