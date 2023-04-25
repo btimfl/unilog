5b9c8beeae96054e438514e14e7865151a8364b4
Raghav <raghav.kanwal@unicommerce.com>
2023-04-25 15:49:34 +0530

    Unilog base API path

--- apis/gateway.ts
+++ apis/gateway.ts
@@ -2,8 +2,8 @@ import Cookies from 'js-cookie'
 import Router from 'next/router'
 import toast from 'react-hot-toast'
 
-// const baseURL = 'http://tenant-unilog.unicommerce.com'
-const baseURL = 'http://localhost:4005'
+const baseURL = 'http://unilog.unicommerce.com'
+// const baseURL = 'http://localhost:4005'
 
 const access_token =
     'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwYXJhZy52eWFzQHVuaWNvbW1lcmNlLmNvbSIsIlVTRVJfREVUQUlMUyI6eyJ1c2VybmFtZSI6InBhcmFnLnZ5YXNAdW5pY29tbWVyY2UuY29tIiwic2Vzc2lvbklkIjoiMmYzYWQ0Y2ItOWI3OC00NmE1LTliZTMtNzAxZmNmMjA5N2E4IiwidGVuYW50IjoidGVuYW50IiwicHJvZHVjdCI6IlNUQU5EQVJEIiwicm9sZSI6IkFETUlOIn0sImV4cCI6MTY4MjA3Njc2NCwiaWF0IjoxNjgxOTkwMzY0fQ.2XitJFTFNE-eU_-O25mxqEPu0VNJ9hjdHpUziIcLXPRIFtkvdlqXScRd1uwA0FIbfOSBHO647N9C-18tLBiqtg'
@@ -14,7 +14,7 @@ defaultHeaders.append('Content-Type', 'application/json')
 defaultHeaders.append('accept', '*/*')
 defaultHeaders.append('Authorization', `Bearer ${access_token}`)
 export default async function gateway(URL: string, options: RequestInit) {
-    if (!Cookies.get('JWT-TOKEN') && URL != 'api/seller/auth_jwt') {
+    if (!Cookies.get('JWT-TOKEN') && URL.indexOf('api/seller/auth_jwt') === -1) {
         toast.error(String('401: Unauthenticated User'))
         Router.push(process.env.NEXT_PUBLIC_HOME_ROUTE!)
         return
