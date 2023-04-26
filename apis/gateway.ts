833fa01d554f7f6f8fd9f725c1152310dcde1300
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-04-26 11:37:55 +0530

    Auth URL changes

--- apis/gateway.ts
+++ apis/gateway.ts
@@ -2,8 +2,9 @@ import Cookies from 'js-cookie'
 import Router from 'next/router'
 import toast from 'react-hot-toast'
 
-// const baseURL = 'http://tenant-unilog.unicommerce.com'
-const baseURL = 'http://localhost:4005'
+// const baseAppURL = 'http://localhost:4005'
+const baseAuthURL = 'http://unilog.unicommerce.com'
+const baseAppURL = 'http://api-unilog.unicommerce.com'
 
 const access_token =
     'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwYXJhZy52eWFzQHVuaWNvbW1lcmNlLmNvbSIsIlVTRVJfREVUQUlMUyI6eyJ1c2VybmFtZSI6InBhcmFnLnZ5YXNAdW5pY29tbWVyY2UuY29tIiwic2Vzc2lvbklkIjoiMmYzYWQ0Y2ItOWI3OC00NmE1LTliZTMtNzAxZmNmMjA5N2E4IiwidGVuYW50IjoidGVuYW50IiwicHJvZHVjdCI6IlNUQU5EQVJEIiwicm9sZSI6IkFETUlOIn0sImV4cCI6MTY4MjA3Njc2NCwiaWF0IjoxNjgxOTkwMzY0fQ.2XitJFTFNE-eU_-O25mxqEPu0VNJ9hjdHpUziIcLXPRIFtkvdlqXScRd1uwA0FIbfOSBHO647N9C-18tLBiqtg'
@@ -21,7 +22,18 @@ export default async function gateway(URL: string, options: RequestInit) {
     } else {
         defaultHeaders.append('JWT-TOKEN', Cookies.get('JWT-TOKEN')!)
     }
-    const res = await fetch(`${baseURL}/${URL}`, {
+    const res = await fetch(`${baseAppURL}/${URL}`, {
+        ...options,
+        headers: defaultHeaders,
+    })
+
+    if (!res.ok) throw new Error(res.statusText)
+    const text = await res.text()
+    return text ? JSON.parse(text) : {}
+}
+
+export async function initAuth(URL: string, options: RequestInit) {
+    const res = await fetch(`${baseAuthURL}/${URL}`, {
         ...options,
         headers: defaultHeaders,
     })
