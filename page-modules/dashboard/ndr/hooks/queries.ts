e34f7299a33396fe13323473d9f81f8d5d51e03b
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-05-02 12:28:33 +0530

    Extract queries in separate component, stop enabling them with missing data, clear conflicts with similar names

---
+++ page-modules/dashboard/ndr/hooks/queries.ts
@@ -0,0 +1,37 @@
+import { useQuery } from '@tanstack/react-query'
+import { FetchNdrReasonSplitType, fetchNdrReasonSplit, fetchNdrShortSummary, fetchNdrStatusSplit } from 'apis/get'
+
+const sanitiseNdrReason = (data: FetchNdrReasonSplitType) => {
+    delete data?.reason_wise_count_details[0].reason
+    return data
+}
+export function useNdrReason(startDate: string, endDate: string) {
+    return useQuery({
+        queryKey: ['fetchNdrReasonSplit', startDate, endDate],
+        queryFn: () => fetchNdrReasonSplit(startDate, endDate),
+        refetchInterval: false,
+        refetchOnWindowFocus: false,
+        enabled: !!startDate && !!endDate,
+        select: sanitiseNdrReason,
+    })
+}
+
+export function useNdrStatus(startDate: string, endDate: string) {
+    return useQuery({
+        queryKey: ['fetchNdrStatusSplit', endDate],
+        queryFn: () => fetchNdrStatusSplit(startDate, endDate),
+        refetchInterval: false,
+        refetchOnWindowFocus: false,
+        enabled: !!startDate && !!endDate,
+    })
+}
+
+export function useNdrShortSummary(startDate: string, endDate: string) {
+    return useQuery({
+        queryKey: ['fetchNdrShortSummary', startDate, endDate],
+        queryFn: () => fetchNdrShortSummary(startDate, endDate),
+        refetchOnWindowFocus: false,
+        refetchInterval: false,
+        enabled: !!startDate && !!endDate,
+    })
+}
