936e997c54e01e7e5a41f7b18ff73144b9945abe
Raghav <raghav.kanwal@unicommerce.com>
2023-05-11 18:15:48 +0530

    enhance authguard

--- shared/components/AuthGuard.tsx
+++ shared/components/AuthGuard.tsx
@@ -1,9 +1,11 @@
-import { Center } from '@chakra-ui/react'
+import { Center, Flex, Icon, Text } from '@chakra-ui/react'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
+import { AiOutlineWarning } from 'react-icons/ai'
 import { useAuthProvider } from 'shared/providers/AuthProvider'
 
-import ErrorPlaceholder from './ErrorPlaceholder/ErrorPlaceholder'
+import accessDeniedImage from './../../public/access-error.png'
 import Loading from './Loading/Loading'
 
 export default function AuthGuard({ children }: { children: ReactNode }) {
@@ -20,8 +22,24 @@ export default function AuthGuard({ children }: { children: ReactNode }) {
     if (allowedURLs.includes('/all_urls') || allowedURLs.includes(router.pathname)) return <>{children}</>
 
     return (
-        <Center w={'100%'} h={'80vh'}>
-            <ErrorPlaceholder message="Feature Not Enabled" />
+        <Center w={'100%'} h={'100%'}>
+            <Flex flexDir="column" gap={4} align="center" justifyContent={`center`}>
+                <Image src={accessDeniedImage} alt={'Access Denied'} width="250"></Image>
+                <Flex justify={`space-between`} align="center" maxW={`450px`}>
+                    <Icon
+                        as={AiOutlineWarning}
+                        fontSize="1.5rem"
+                        fontWeight="normal"
+                        display="inline-block"
+                        verticalAlign="middle"
+                        ms={6}
+                    />
+                    <Text w={`85%`} fontSize="sm">
+                        You seem to lack the privileges required to access this content. To fix this, please contact the
+                        account administrator.
+                    </Text>
+                </Flex>
+            </Flex>
         </Center>
     )
 }
